429ea027430c4e209819c5c8cddec86ddef6eb17
qinniiinu <cinnnk@gmail.com>
2024-09-30 11:19:24 +0800

    建立

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { WebSocketService } from './app/web-socket.service';
+import { map } from 'rxjs/internal/operators/map';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+    <h1>Hello </h1>
+    <button (click)="start()">啟動webSocket</button>
   `,
 })
 export class App {
   name = 'Angular';
+
+  messageCount = 0
+  constructor(public socketService: WebSocketService) { }
+
+  start() {
+    this.socketService.createObservableSocket('ws://localhost:8000')
+    .pipe(map(event => JSON.parse(event))
+    )
+    .subscribe(
+        event => this.messageCount = event.messageCount
+      )
+  }
+
 }
 
 bootstrapApplication(App);
